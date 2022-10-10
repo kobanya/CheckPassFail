cc27c53b310a8309d9f76135bfef8d105a54957f
kobanya <nagy.bela.budapest@gmail.com>
2022-10-10 12:03:49 +0200

    a kód

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class Main {  // Save as "CheckPassFail.java"
+    public static void main(String[] args) {  // Program entry point
+        int mark = 49;   // Set the value of "mark" here!
+        System.out.println("The mark is " + mark);
+
+        // if-else statement
+        if (mark >= 50) {
+            System.out.println("Pass / Átment");
+        } else {
+            System.out.println("FAIL / Nem ment át");
+        }
+        System.out.println("DONE / Kész");
     }
 }
